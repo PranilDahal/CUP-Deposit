20d7f84d4300569721dcee60ca50dd8cd00fc0cd
Wesley <whwang2@calstatela.edu>
2019-02-09 23:58:16 -0800

    html for balancebyplan

--- frontend/src/app/balancebyplan/balancebyplan.component.ts
+++ frontend/src/app/balancebyplan/balancebyplan.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BalancebyplanComponent implements OnInit {
 
-  constructor() { }
+   balancebyplan:any = [] ;
+  constructor(public rest:RestapiService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+	  this.getBalanceByPlan();
+  }
+  
+  getBalanceByPlan() {
+    this.balancebyplan = [];
+    this.rest.getBalancesByPlan().subscribe((data: {}) => {
+      console.log(data);
+      this.balancebyplan = data;
+    });
   }
 
 }
