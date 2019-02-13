08b0a839d7463936fdb0b0a4e0a8d085d5d6dce9
vtang4 <vtang1220@gmail.com>
2019-02-13 11:31:50 -0800

    all account balances

--- frontend/src/app/allaccountbalances/allaccountbalances.component.ts
+++ frontend/src/app/allaccountbalances/allaccountbalances.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestapiService } from '../restapi.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-allaccountbalances',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllaccountbalancesComponent implements OnInit {
 
-  constructor() { }
+  allaccountbalances:any = [] ;
+  constructor(public rest:RestapiService, private route: ActivatedRoute, private router: Router) { }
 
+  // Function that gets called when the componenet is initialized
   ngOnInit() {
+    this.getAllAccountBalances();
   }
+
+  getAllAccountBalances() {
+    this.allaccountbalances = [];
+    this.rest.getAllAccountBalances().subscribe((data: {}) => {
+      console.log(data);
+      this.allaccountbalances = data;
+    });
+  }
+
 
 }
